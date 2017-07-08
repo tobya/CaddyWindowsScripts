ab656b38aca6d96ea24022ec34cbc8965549cd7b
Toby Allen <tobyallen@toflidium.com>
2017-07-08 17:03:28 +0100

    Update shutdownCaddy.bat

--- shutdownCaddy.bat
+++ shutdownCaddy.bat
@@ -1,2 +1,3 @@
+REM This script should be called on a regular (hours) basis to reset the php fast-cgi processes (caddy reloads to achieve this)
 ECHO Shuting down CADDY and subprocesses (fastcgi)
 taskkill /IM caddy.exe /f /t
